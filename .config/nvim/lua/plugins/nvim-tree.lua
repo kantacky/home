2e06b213a6298358c45316552c8361c0526c62b2
Kanta Oikawa <iam@kantacky.com>
2026-01-05 21:12:23 +0900

    Add option of nvim-tree

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,7 +1,24 @@
 return {
   {
     "nvim-tree/nvim-tree.lua",
-    opts = {},
+    opts = {
+      -- git関連の設定
+      git = {
+        enable = true,
+        -- trueだとgitignoreを隠す、falseだと表示する
+        ignore = false,
+      },
+      -- フィルタ関連の設定
+      filters = {
+        -- ドットファイル（.env等）も表示したい場合はここをfalseに
+        dotfiles = false,
+      },
+      -- その他、お好みの設定をここに追加
+      view = {
+        width = 30,
+        side = "left",
+      },
+    },
   },
 }
 
