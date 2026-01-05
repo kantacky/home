68f0cb29a395e42fc6bbb1d54876fcc057c0e2aa
Kanta Oikawa <iam@kantacky.com>
2026-01-05 13:50:10 +0900

    Add nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,20 @@
+-- クリップボードを同期
+vim.opt.clipboard = "unnamedplus"
+-- 行番号を表示
+vim.opt.number = true
+-- タブ文字の代わりにスペースを使う
+vim.opt.expandtab = true
+-- プログラミング言語に合わせて適切にインデントを自動挿入
+vim.opt.smartindent = true
+-- 各コマンドやsmartindentで挿入する空白の量
+vim.opt.shiftwidth = 4
+-- Tabキーで挿入するスペースの数
+vim.opt.softtabstop = 4
+
+require("config.lazy")
+
+-- Keymap
+vim.keymap.set("n", "t", ":NvimTreeToggle<Enter>")
+vim.keymap.set("n", "\\", ":vs<Enter>")
+vim.keymap.set("n", "-", ":sp<Enter>")
+
